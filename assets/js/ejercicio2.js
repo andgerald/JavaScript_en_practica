edb68c6a8a67b8894ca5e4d49c552f2b47e62030
andgeral <geraldinebecerra93@gmail.com>
2024-02-18 14:30:25 -0300

    ejercicio 2

---
+++ assets/js/ejercicio2.js
@@ -0,0 +1,30 @@
+let caja = document.getElementById("caja");
+
+const buttonRed = document.getElementById("btn-1");
+buttonRed.addEventListener("click", function () {
+  caja.style.backgroundColor = "#e53e3e ";
+});
+
+const buttonOrange = document.getElementById("btn-2");
+buttonOrange.addEventListener("click", function () {
+  caja.style.backgroundColor = "#dd6b20 ";
+});
+
+const buttonYellow = document.getElementById("btn-3");
+buttonYellow.addEventListener("click", function () {
+  caja.style.backgroundColor = "#faf089 ";
+});
+
+const buttonGreen = document.getElementById("btn-4");
+buttonGreen.addEventListener("click", function () {
+  caja.style.backgroundColor = "#48bb78 ";
+});
+
+const buttonSlyblue = document.getElementById("btn-5");
+buttonSlyblue.addEventListener("click", function () {
+  caja.style.backgroundColor = "#81e6d9 ";
+});
+const buttonPink = document.getElementById("btn-6");
+buttonPink.addEventListener("click", function () {
+  caja.style.backgroundColor = "#d53f8c ";
+});
